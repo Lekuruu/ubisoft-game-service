c4b46d3e79b1fe7cec8b8bb1993bde6651e6aa2b
Lekuru <contact@lekuru.xyz>
2024-08-23 22:21:01 +0200

    Implement key exchange handler

--- app/services/router/handlers.py
+++ app/services/router/handlers.py
@@ -20,3 +20,26 @@ def register(type: MessageType):
 @register(MessageType.STILLALIVE)
 def still_alive(message: Message, client: RouterProtocol):
     client.send(bytes(message))
+
+@register(MessageType.KEY_EXCHANGE)
+def key_exchange(message: Message, client: RouterProtocol):
+    request_id = message.data.lst[0]
+
+    match request_id:
+        case '1':
+            client.game_pubkey = RsaPublicKey.from_buf(message.data.lst[1][2]).to_pubkey()
+            pub_key, priv_key = pkc.keygen()
+            client.sv_pubkey = pub_key
+            client.sv_privkey = priv_key
+            response = gsm.KeyExchangeResponse(message, client)
+
+        case '2':
+            enc_bf_key = bytes(message.data.lst[1][2])
+            bf_key = pkc.decrypt(enc_bf_key, client.sv_privkey)
+            client.game_bf_key = bf_key
+            response = gsm.KeyExchangeResponse(message, client)
+
+        case _:
+            raise NotImplementedError(f"Unknown requestId: {request_id}")
+
+    client.send(bytes(response))
