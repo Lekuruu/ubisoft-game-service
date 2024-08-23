fba4e80b761cbd79e644eecbe58e9b962ae93fbb
Lekuru <contact@lekuru.xyz>
2024-08-23 20:25:09 +0200

    Add initial `RouterProtocol` class

---
+++ app/services/router/protocol.py
@@ -0,0 +1,5 @@
+
+from app.services.protocol import BaseProtocol
+
+class RouterProtocol(BaseProtocol):
+    ...
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from app.services.server import Server, HttpServer
+from app.services.router import RouterProtocol
 from app.services.gsconnect import GSConnect
 from app.logging import ConsoleLogger
 from twisted.internet import reactor
@@ -13,6 +14,7 @@ logging.basicConfig(
 
 def main():
     HttpServer('GSConnect', 80, GSConnect()).start()
+    Server('Router', 40000, RouterProtocol).start()
     reactor.run()
 
 if __name__ == '__main__':
