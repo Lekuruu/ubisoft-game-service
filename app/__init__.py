232b160506cad50e86707141328b1cb0fd4bad51
Lekuru <contact@lekuru.xyz>
2024-08-23 19:07:15 +0200

    Add twisted base protocol

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+
+from . import services
+from . import logging
---
+++ app/services/server.py
@@ -0,0 +1,61 @@
+
+from __future__ import annotations
+
+from twisted.internet.address import IPv4Address, IPv6Address
+from twisted.internet.protocol import Protocol
+from twisted.internet.error import ConnectionDone
+from twisted.python.failure import Failure
+
+import logging
+
+IPAddress = IPv4Address | IPv6Address
+
+class BaseProtocol(Protocol):
+    """
+    Base protocol class that includes basic logging, as well as
+    providing functions that follow conventional naming schemes.
+    """
+
+    def __init__(self, address: IPAddress) -> None:
+        self.host = address.host
+        self.port = address.port
+        self.logger = logging.getLogger(self.host)
+
+    def on_connect(self) -> None:
+        ...
+
+    def on_disconnect(self, reason: Failure) -> None:
+        ...
+
+    def on_data(self, data: bytes) -> None:
+        ...
+
+    def send(self, data: bytes):
+        try:
+            self.transport.write(data)
+        except Exception as e:
+            self.logger.error(
+                f'Could not write to transport layer: {e}',
+                exc_info=True
+            )
+
+    def disconnect(self) -> None:
+        self.logger.debug(f'-> Closing connection...')
+        self.transport.loseConnection()
+        self.on_disconnect(ConnectionDone())
+
+    def dataReceived(self, data: bytes) -> None:
+        self.on_data(data)
+
+    def connectionMade(self):
+        self.logger.debug(f'-> <{self.host}:{self.port}>')
+        self.on_connect()
+
+    def connectionLost(self, reason: Failure = ConnectionDone()) -> None:
+        if reason.type is ConnectionDone:
+            self.logger.debug(f'-> Connection done.')
+            self.on_disconnect(reason)
+            return
+
+        self.logger.warning(f'-> Lost connection "{reason.getErrorMessage()}".')
+        self.on_disconnect(reason)
