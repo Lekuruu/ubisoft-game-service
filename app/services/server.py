a59c16087a3959c1c1c20254fb7f466ad7ecdb57
Lekuru <contact@lekuru.xyz>
2024-08-23 19:52:55 +0200

    Add base `HttpServer` class

--- app/services/server.py
+++ app/services/server.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 
 from app.services.protocol import BaseProtocol
 from twisted.internet.protocol import Factory
+from twisted.web.resource import Resource
 from twisted.internet import reactor
+from twisted.web.server import Site
 
 import logging
 
@@ -30,3 +32,24 @@ class Server(Factory):
 
     def buildProtocol(self, address):
         return self.protocol(address)
+
+class HttpServer(Site):
+    def __init__(
+        self,
+        name: str,
+        port: int,
+        root: Resource
+    ) -> None:
+        super().__init__(root)
+        self.name = name
+        self.port = port
+        self.logger = logging.getLogger(name)
+
+    def start(self) -> None:
+        reactor.listenTCP(self.port, self)
+
+    def startFactory(self):
+        self.logger.info(f'Listening on {self.port}...')
+
+    def stopFactory(self):
+        self.logger.warning(f'Stopping server...')
