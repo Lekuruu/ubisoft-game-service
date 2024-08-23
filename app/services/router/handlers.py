3b155461d242ac3749c57bdeb7342d01d6612bc2
Lekuru <contact@lekuru.xyz>
2024-08-23 22:20:49 +0200

    Implement still alive handler

--- app/services/router/handlers.py
+++ app/services/router/handlers.py
@@ -1,8 +1,14 @@
 
-from app.constants import MessageType
+from __future__ import annotations
 from typing import Callable
 
-# Global dictionary to store message handler functions
+from app.services.router import RouterProtocol
+from app.utils.pkc import RsaPublicKey
+from app.constants import MessageType
+from app.utils.gsm import Message
+from app.utils import pkc, gsm
+
+"""Global dictionary to store message handler functions"""
 MessageTypeHandlers = {}
 
 def register(type: MessageType):
@@ -10,3 +16,7 @@ def register(type: MessageType):
         MessageTypeHandlers[type] = func
         return func
     return decorator
+
+@register(MessageType.STILLALIVE)
+def still_alive(message: Message, client: RouterProtocol):
+    client.send(bytes(message))
