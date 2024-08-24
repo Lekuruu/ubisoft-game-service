4ac3a3428410ea20e8fa36ee40ce7448ac60f1ac
Lekuru <contact@lekuru.xyz>
2024-08-24 20:08:52 +0200

    Add base cdkey service

---
+++ app/services/cdkey/protocol.py
@@ -0,0 +1,6 @@
+
+from __future__ import annotations
+from app.services.udp import BaseUdpProtocol, IPAddress
+
+class CDKeyProtocol(BaseUdpProtocol):
+    ...
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 
-from app.services.router import RouterProtocol
-from app.services.gsconnect import GSConnect
-from app.services.cdkey import CDKeyProtocol
-from app.services.http import HttpServer
-from app.services.tcp import TcpServer
-from app.logging import ConsoleLogger
+from __future__ import annotations
+
+from twisted.internet.protocol import Protocol, DatagramProtocol
+from twisted.web.resource import Resource
 from twisted.internet import reactor
+
+from app.logging import ConsoleLogger
+from app.services import (
+    RouterProtocol,
+    CDKeyProtocol,
+    HttpServer,
+    TcpServer,
+    GSConnect
+)
 
 import logging
 import app
@@ -15,12 +22,32 @@ logging.basicConfig(
     handlers=[ConsoleLogger]
 )
 
+def listen_tcp(port: int, protocol: Protocol) -> None:
+    TcpServer(
+        protocol.__name__,
+        port,
+        protocol
+    ).start()
+
+def listen_http(port: int, resource: Resource) -> None:
+    HttpServer(
+        resource.__name__,
+        port,
+        resource()
+    ).start()
+
+def listen_udp(port: int, protocol: DatagramProtocol) -> None:
+    reactor.listenUDP(
+        port,
+        protocol()
+    )
+
 def main() -> None:
     Services = app.config['services']
-    HttpServer('GSConnect', Services['GSConnect']['Port'], GSConnect()).start()
-    TcpServer('WaitModule', Services['Router']["WaitModule"]['Port'], RouterProtocol).start()
-    TcpServer('Router', Services['Router']['Port'], RouterProtocol).start()
-    TcpServer('CDKey', Services['CDKey']['Port'], CDKeyProtocol).start()
+    listen_http(Services['GSConnect']['Port'], GSConnect)
+    listen_tcp(Services['Router']["WaitModule"]['Port'], RouterProtocol)
+    listen_tcp(Services['Router']['Port'], RouterProtocol)
+    listen_udp(Services['CDKey']['Port'], CDKeyProtocol)
     reactor.run()
 
 if __name__ == '__main__':
