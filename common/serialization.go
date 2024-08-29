2d3be39bd3dd1c9faed9065c970036117ef8dbd8
Lekuru <contact@lekuru.xyz>
2024-08-29 17:16:05 +0200

    Add integer generics to int16/int32 writing

--- common/serialization.go
+++ common/serialization.go
@@ -3,27 +3,20 @@ package common
 import (
 	"encoding/binary"
 	"math/big"
+
+	"golang.org/x/exp/constraints"
 )
 
-func WriteU32(value int) []byte {
+func WriteU32[T constraints.Integer](value T) []byte {
 	result := make([]byte, 4)
 	binary.LittleEndian.PutUint32(result, uint32(value))
 	return result
 }
 
-func WriteU16(v int) []byte {
+func WriteU16[T constraints.Integer](value T) []byte {
 	buf := make([]byte, 2)
-	binary.LittleEndian.PutUint16(buf, uint16(v))
+	binary.LittleEndian.PutUint16(buf, uint16(value))
 	return buf
-}
-
-func ReadAsU32List(buf []byte) []uint32 {
-	size := len(buf) / 4
-	result := make([]uint32, size)
-	for i := 0; i < size; i++ {
-		result[i] = binary.LittleEndian.Uint32(buf[i*4:])
-	}
-	return result
 }
 
 func WriteU32List(ints []uint32) []byte {
@@ -32,6 +25,15 @@ func WriteU32List(ints []uint32) []byte {
 		binary.LittleEndian.PutUint32(buf[i*4:], val)
 	}
 	return buf
+}
+
+func ReadAsU32List(data []byte) []uint32 {
+	size := len(data) / 4
+	result := make([]uint32, size)
+	for i := 0; i < size; i++ {
+		result[i] = binary.LittleEndian.Uint32(data[i*4:])
+	}
+	return result
 }
 
 func WriteBigIntBE(value *big.Int, length int) []byte {
