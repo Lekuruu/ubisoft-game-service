bca72b66b704447b89ae2c5ffb093ca5dc175f15
Lekuru <contact@lekuru.xyz>
2024-08-30 18:03:13 +0200

    Implement cdkey challenge response

--- cdkey/handlers.go
+++ cdkey/handlers.go
@@ -1,4 +1,30 @@
 package cdkey
 
+import (
+	"strconv"
+
+	"github.com/lekuruu/ubisoft-game-service/router"
+)
+
 // A map to store the handlers for each message type
-var CDKeyHandlers = map[uint8]func(*CDKeyMessage, *Client) (*CDKeyMessage, error){}
+var CDKeyHandlers = map[int]func(*CDKeyMessage, *Client) (*CDKeyMessage, error){}
+
+func handleChallenge(msg *CDKeyMessage, client *Client) (*CDKeyMessage, error) {
+	response := NewCDKeyMessageFromRequest(msg)
+	hash := []byte{
+		0x00, 0x11, 0x22, 0x33,
+		0x44, 0x55, 0x66, 0x77,
+		0x88, 0x99, 0xaa, 0xbb,
+		0xcc, 0xdd, 0xee, 0xff,
+		0x01, 0x02, 0x03, 0x04,
+	}
+	response.Data[3] = []interface{}{
+		strconv.Itoa(router.GSM_GSSUCCESS),
+		hash,
+	}
+	return response, nil
+}
+
+func init() {
+	CDKeyHandlers[CDKM_CHALLENGE] = handleChallenge
+}
