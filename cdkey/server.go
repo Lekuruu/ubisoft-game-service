75c7590eb129409b2ff53d358a4a14bb65ec2095
Lekuru <contact@lekuru.xyz>
2024-08-30 18:51:08 +0200

    Lower the amount of bytes read from cdkey clients

--- cdkey/server.go
+++ cdkey/server.go
@@ -36,7 +36,7 @@ func (cdks *CDKeyServer) Serve() {
 	defer listener.Close()
 
 	for {
-		buffer := make([]byte, 1024)
+		buffer := make([]byte, 512)
 		_, addr, err := listener.ReadFrom(buffer)
 
 		if err != nil {
