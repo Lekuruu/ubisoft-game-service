45f1cfe43433cf508f50696aa8b181528836f3d4
Lekuru <contact@lekuru.xyz>
2024-08-30 19:20:58 +0200

    Implement gsinit route

--- gsconnect/gsinit.go
+++ gsconnect/gsinit.go
@@ -2,8 +2,30 @@ package gsconnect
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func GSInitRoute(gsc *GSContext) {
-	fmt.Fprintf(gsc.Response, "GSInitRoute") // TODO
+	query := gsc.Request.URL.Query()
+	user := query.Get("user")
+	product := query.Get("dp")
+	gsc.Response.Header().Add("Content-Type", "text/plain")
+
+	if product == "" {
+		gsc.Response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	game, ok := gsc.Server.Games[product]
+
+	if !ok {
+		gsc.Response.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	log := fmt.Sprintf("'%s' is connecting to '%s'", user, product)
+	gsc.Server.Logger.Info(log)
+
+	gsc.Response.WriteHeader(http.StatusOK)
+	gsc.Response.Write([]byte(game))
 }
