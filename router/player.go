3eb069402fa49335df5803b7b48548b5e5364eb4
Lekuru <contact@lekuru.xyz>
2024-09-01 23:24:32 +0200

    Add more player info & `IpAddress` function to player struct

--- router/player.go
+++ router/player.go
@@ -1,9 +1,20 @@
 package router
 
+import "strings"
+
 type Player struct {
 	Client
-	Id   int
-	Name string
-	Nick string
-	Game string
+	Id        int
+	Name      string
+	Firstname string
+	Surname   string
+	Country   string
+	Email     string
+	Game      string
+	Version   string
+	Public    bool
+}
+
+func (player *Player) IpAddress() string {
+	return strings.Split(player.Client.Conn.RemoteAddr().String(), ":")[0]
 }
